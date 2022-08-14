3208427e80439e383ea85b77b0bac07c2794dc3c
The_Beast <windows9.1@hotmail.com>
2022-08-14 00:09:46 -0400

    subiendo

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var texto=document.querySelector("textarea");
+var boton = document.querySelector("#btnLeer");
+var btnCopiar = document.querySelector(".btnCopiar");
+var btnPegar = document.querySelector(".btnPegar");
+
+function escuchar(){
+    var msg = texto.value;
+    var utterance = new SpeechSynthesisUtterance(msg);
+    speechSynthesis.speak(utterance);
+
+}
+function copiar(){
+    texto.select();
+    document.execCommand('copy');
+}
+
+async function  pegar  (){
+     
+    var textoPegar= await navigator.clipboard.readText();
+    texto.value=textoPegar;
+    
+}
+boton.onclick=escuchar;
+btnCopiar.onclick=copiar;
+btnPegar.onclick=pegar;
+
